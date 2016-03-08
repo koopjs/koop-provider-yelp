fb15b6e23c25b1ba08dcd49b5ba66cd2c210bcc9
dmfenton <dfenton@esri.com>
2016-03-07 19:26:28 -0500

    rewrite as pass-thru

--- controller/index.js
+++ controller/index.js
@@ -1,74 +1,30 @@
-var hash = require('object-hash');
-var _ = require('underscore');
-
-var Controller = function( yelp, BaseController ){
-
-  // inherit from the base controller to share some logic 
-  var controller = {};
-  controller.__proto__ = BaseController();
-
+const Controller = function (yelp, createBaseController) {
+  const controller = createBaseController()
   // respond to the root route
-  controller.index = function(req, res){
-    res.send('This is the base route of ye Yelp provider');
-  };
+  controller.index = function (req, res) {
+    res.send('This is the base route of ye Yelp provider')
+  }
 
-  // get a resource from the providers model 
-  controller.get = function(req, res){
-    yelp.find(req.params, req.query, function(err, data){
-      if (err){
-        res.send(err, 500);
-      } else {
-        res.json( data );
-      }
-    });
-  };
-  
   // use the shared code in the BaseController to create a feature service
-  controller.featureserver = function(req, res){
-    var callback = req.query.callback, self = this;
-    delete req.query.callback;
+  controller.featureServer = function (req, res) {
+    const callback = req.query.callback
 
     // need to always respond with a count greater than the max count of 1000
     // that way the JS API will have to ask for bounding boxes
-    if(req.query.returnCountOnly === 'true'){
-      res.send({count: 1001});
-    } else {
-      yelp.find(req.params, req.query, function(err, data){
-        if (err) {
-          res.send(err, 500);
-        } else {
-          // inherited logic for processing feature service requests      
-          controller.processFeatureServer( req, res, err, data, callback);
-        }
-      });
-    }
-  };
+    if (req.query.returnCountOnly === 'true') return res.send({count: 1001})
+    yelp.search(req.query, function (err, geojson) {
+      if (err) return res.status(500).send(err)
+      controller.processFeatureServer(req, res, err, geojson, callback)
+    })
+  }
 
-  // render templates and views 
-  controller.preview = function(req, res){
-    res.render(__dirname + '/../views/demo', { locals:{ id: req.params.id } });
-  };
+  // render templates and views
+  controller.preview = function (req, res) {
+    res.render(__dirname + '/../views/demo', { locals: { id: req.params.id } })
+  }
 
-  // drops the cache
-  controller.drop = function(req, res){
-    var params = req.params;
-    params.layer = 0;
-
-    var key = hash.MD5(_.omit(params,'method'));
-
-    yelp.drop( key, req.query, function(error, result){
-      if (error) {
-        res.send( error, 500);
-      } else {
-        res.json( result );
-      }
-    });
-  };
-  
   // return the controller so it can be used by koop
-  return controller;
+  return controller
+}
 
-};
-
-module.exports = Controller;
-
+module.exports = Controller
--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // the name of provider is used by koop to help build default routes for FeatureService and a preview
-exports.name = 'yelp';
+const pkg = require('./package.json')
+const provider = {
+  plugin_name: 'yelp',
+  hosts: false,
+  controller: require('./controller'),
+  model: require('./models/yelp'),
+  routes: require('./routes'),
+  status: {
+    version: pkg.version
+  },
+  type: 'provider'
+}
 
-// the "pattern" is used to build routes and is optional 
-// a pattern is essentially the default route pattern for the provider 
-exports.pattern = '/:term';
-
-// attached the controller to the provider 
-exports.controller = require('./controller');
-
-// attaches the routes file to the provider 
-exports.routes = require('./routes');
-
-// attaches the model to the provider to be passed into the controller at start up time 
-exports.model = require('./models/yelp.js');
+module.exports = provider
