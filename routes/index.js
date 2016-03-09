e52a142ae6624588795fbf2fef2f9150b34aff0a
dmfenton <dfenton@esri.com>
2016-03-09 08:49:01 -0800

    add layer as variable in routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const routes = {
   'get /yelp': 'index',
-	'get /yelp/FeatureServer': 'featureServer',
-  'get /yelp/FeatureServer/0': 'featureServer',
-  'get /yelp/FeatureServer/0/:method': 'featureServer',
-  'post /yelp/FeatureServer/0/:method': 'featureServer'
+  'get /yelp/FeatureServer': 'featureServer',
+  'get /yelp/FeatureServer/:layer': 'featureServer',
+  'get /yelp/FeatureServer/:layer/:method': 'featureServer',
+  'post /yelp/FeatureServer/:layer/:method': 'featureServer'
 }
 
 module.exports = routes
