7741395b4a8e4920571775e88e28a0cc0321eacf
dmfenton <dfenton@esri.com>
2016-03-10 09:17:33 -0800

    streamline and comment

--- controller/index.js
+++ controller/index.js
@@ -2,7 +2,7 @@ const Controller = function (yelp, createBaseController) {
   const controller = createBaseController()
   // respond to the root route
   controller.index = function (req, res) {
-    res.send('This is the base route of ye Yelp provider')
+    res.send('Hello Yelp')
   }
 
   // use the shared code in the BaseController to create a feature service
@@ -10,15 +10,17 @@ const Controller = function (yelp, createBaseController) {
     // need to always respond with a count greater than the max count of 1000
     // that way the JS API will have to ask for bounding boxes
     if (req.query.returnCountOnly === 'true') return res.send({count: 1001})
+   
+
+		// Fetch data from Yelp's API and translate it into geojson
     yelp.search(req.query, function (err, geojson) {
       if (err) return res.status(500).send(err)
+      // we want to handle filtered and ordering on our own, so we tell Koop to skip these steps
+  		req.query.skipFilter = true
+      req.query.skipOrder = true
+      // hand things off to Koop's Feature Server controller
       controller.processFeatureServer(req, res, geojson)
     })
-  }
-
-  // render templates and views
-  controller.preview = function (req, res) {
-    res.render(__dirname + '/../views/demo', { locals: { id: req.params.id } })
   }
 
   // return the controller so it can be used by koop
--- models/yelp.js
+++ models/yelp.js
@@ -1,6 +1,3 @@
-// used to send http requests
-// used to manage control flow
-// used to create hashes that fingerprint a given request
 const YelpClient = require('yelp')
 const proj4 = require('proj4')
 const proj = proj4('GOOGLE', 'WGS84')
@@ -8,7 +5,10 @@ const config = require('config')
 const _ = require('lodash')
 const Yelp = function (koop) {
   var yelp = {}
+
   const client = new YelpClient(config.yelp)
+  
+  // This is our one public function it's job its to fetch data from Yelp and return GeoJSON
   yelp.search = function (query, callback) {
     getFromYelp(query, function (err, rawResponse) {
       if (err) return callback(err)
@@ -16,38 +16,72 @@ const Yelp = function (koop) {
       callback(null, featureCollection)
     })
   }
-
+  
+	// Wrap the call to Yelp, this will make testing easier and decouple us from the specific client lib
   function getFromYelp (options, callback) {
-    // simple wrapper around requests to the desired API
     const query = buildQuery(options)
     client.search(query, function (err, data) {
       callback(err, data)
     })
   }
-
+  
+	// Translate a request from the GeoServices API into something Yelp will understand
   function buildQuery (options) {
-    // create a a default set of parameters for the API call
-    // fill in passed in parameters where available
+    // we don't want to modify the passed in options object because Koop will use that later
+		// also for some reason the Yelp lib chokes when there is a passed in callback so omit it
     const query = _.omit(_.cloneDeep(options), 'callback')
     if (options.geometry) {
-      const bbox = JSON.parse(options.geometry)
-      query.bounds = proj.forward([bbox.xmin, bbox.ymin]).reverse().toString() + '|' + proj.forward([bbox.xmax, bbox.ymax]).reverse().toString()
+      query.bounds = setBounds(options)
     } else if (!options.location) {
       query.location = 'Washington, DC'
     }
-    if (!query.sort) options.sort = 1
+    query.term = setTerm(options)
+    query.sort = setSort(options)
+    query.limit = 20
     return query
   }
 
-  function translate (data) {
-    if (!data.businesses) return []
-    const features = data.businesses.map(formatFeature)
-    return {
-      type: 'FeatureCollection',
-      features: features
+	// This function translates a geoservices geometry query the Yelp APIs weird format
+  function setBounds (options) {
+    const bbox = JSON.parse(options.geometry)
+    return proj.forward([bbox.xmin, bbox.ymin]).reverse().toString() + '|' + proj.forward([bbox.xmax, bbox.ymax]).reverse().toString()
+  }
+
+	// This function parses the search term from a geoservices where clause
+	// 'where term = 'restaurants' => restaurants
+  function setTerm (options) {
+    if (!options.where) return null
+    const match = options.where.match(/.+\s+=\s+'(.+)'/)
+    return match ? match[1] : null
+  }
+
+	// This function handles the sort part of yelps query. Depending on the type of search we are doing
+	// we may want to sort by distance, rating or best match
+  function setSort (options) {
+    if (options.orderByFields && options.orderByFields === 'rating') {
+      // Highest rated
+      return 2
+    } else if (options.term) {
+      // Best match
+      return 0
+    } else {
+      // Distance
+      return 1
     }
   }
 
+	// Map accross all elements from a Yelp respsonse and translate it into a feature collection
+  function translate (data) {
+    const featureCollection = {
+      type: 'FeatureCollection',
+			features: []
+		}
+		// protect ourself in case the request did not return any features
+		if (data.businesses) featureCollection.features = data.businesses.map(formatFeature)
+    return featureCollection
+  }
+
+	// This function takes a single element from the yelp response and translates it to GeoJSON
   function formatFeature (biz) {
     const loc = biz.location
     return {
@@ -71,7 +105,8 @@ const Yelp = function (koop) {
         yelp_page: biz.url,
         rating_img: biz.rating_img_url_small,
         yelp_id: biz.id,
-        is_closed: biz.is_closed
+        is_closed: biz.is_closed,
+        term: 'String' //We put a dummy term in here so ArcGIS knows this is a string field. It will allow us to filter 
       }
     }
   }
