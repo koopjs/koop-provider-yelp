35ba4220d9885338b2519352488bac7c020ddd7f
dmfenton <dfenton@esri.com>
2016-03-11 19:00:27 -0500

    try multiple queries

--- controller/index.js
+++ controller/index.js
@@ -10,13 +10,11 @@ const Controller = function (yelp, createBaseController) {
     // need to always respond with a count greater than the max count of 1000
     // that way the JS API will have to ask for bounding boxes
     if (req.query.returnCountOnly === 'true') return res.send({count: 1001})
-   
-
 		// Fetch data from Yelp's API and translate it into geojson
     yelp.search(req.query, function (err, geojson) {
       if (err) return res.status(500).send(err)
       // we want to handle filtered and ordering on our own, so we tell Koop to skip these steps
-  		req.query.skipFilter = true
+      req.query.skipFilter = true
       req.query.skipOrder = true
       // hand things off to Koop's Feature Server controller
       controller.processFeatureServer(req, res, geojson)
--- models/yelp.js
+++ models/yelp.js
@@ -3,35 +3,61 @@ const proj4 = require('proj4')
 const proj = proj4('GOOGLE', 'WGS84')
 const config = require('config')
 const _ = require('lodash')
+const async = require('async')
 const Yelp = function (koop) {
   var yelp = {}
 
   const client = new YelpClient(config.yelp)
-  
+
   // This is our one public function it's job its to fetch data from Yelp and return GeoJSON
-  yelp.search = function (query, callback) {
-    getFromYelp(query, function (err, rawResponse) {
+  yelp.search = function (options, callback) {
+    const queries = buildQueries(options)
+    const featureCollection = {
+      type: 'FeatureCollection',
+      features: []
+    }
+
+    async.each(queries, search, finish)
+
+    function search (query, callback) {
+      searchYelp(query, function (err, features) {
+        if (err) return callback(err)
+        featureCollection.features = featureCollection.features.concat(features)
+        callback()
+      })
+    }
+
+    function finish (err) {
+      callback(err, featureCollection)
+    }
+  }
+
+  // Wrap the call to Yelp, this will make testing easier and decouple us from the specific client lib
+  function searchYelp (query, callback) {
+    client.search(query, function (err, rawResponse) {
       if (err) return callback(err)
-      const featureCollection = translate(rawResponse)
-      callback(null, featureCollection)
+      const features = translate(rawResponse)
+      callback(null, features)
     })
   }
-  
-	// Wrap the call to Yelp, this will make testing easier and decouple us from the specific client lib
-  function getFromYelp (options, callback) {
-    const query = buildQuery(options)
-    client.search(query, function (err, data) {
-      callback(err, data)
-    })
-  }
-  
-	// Translate a request from the GeoServices API into something Yelp will understand
-  function buildQuery (options) {
-    // we don't want to modify the passed in options object because Koop will use that later
-		// also for some reason the Yelp lib chokes when there is a passed in callback so omit it
-    const query = _.omit(_.cloneDeep(options), 'callback')
+
+  function buildQueries (options) {
     if (options.geometry) {
-      query.bounds = setBounds(options)
+      const geometries = splitGeometry(options.geometry)
+      return geometries.map(geometry => buildQuery(options, geometry))
+    } else {
+      return [buildQuery(options)]
+    }
+  }
+
+  // Translate a request from the GeoServices API into something Yelp will understand
+  function buildQuery (options, geometry) {
+    // we don't want to modify the passed in options object because Koop will use that later
+    // also for some reason the Yelp lib chokes when there is a passed in callback so omit it
+    const query = _.omit(_.cloneDeep(options), 'callback')
+    if (geometry || options.geometry) {
+      const bbox = geometry || JSON.parse(options.geometry)
+      query.bounds = setBounds(bbox)
     } else if (!options.location) {
       query.location = 'Washington, DC'
     }
@@ -41,22 +67,21 @@ const Yelp = function (koop) {
     return query
   }
 
-	// This function translates a geoservices geometry query the Yelp APIs weird format
-  function setBounds (options) {
-    const bbox = JSON.parse(options.geometry)
+  // This function translates a geoservices geometry query the Yelp APIs weird format
+  function setBounds (bbox) {
     return proj.forward([bbox.xmin, bbox.ymin]).reverse().toString() + '|' + proj.forward([bbox.xmax, bbox.ymax]).reverse().toString()
   }
 
-	// This function parses the search term from a geoservices where clause
-	// 'where term = 'restaurants' => restaurants
+  // This function parses the search term from a geoservices where clause
+  // 'where term = 'restaurants' => restaurants
   function setTerm (options) {
     if (!options.where) return null
     const match = options.where.match(/.+\s+=\s+'(.+)'/)
     return match ? match[1] : null
   }
 
-	// This function handles the sort part of yelps query. Depending on the type of search we are doing
-	// we may want to sort by distance, rating or best match
+  // This function handles the sort part of yelps query. Depending on the type of search we are doing
+  // we may want to sort by distance, rating or best match
   function setSort (options) {
     if (options.orderByFields && options.orderByFields === 'rating') {
       // Highest rated
@@ -70,18 +95,13 @@ const Yelp = function (koop) {
     }
   }
 
-	// Map accross all elements from a Yelp respsonse and translate it into a feature collection
+  // Map accross all elements from a Yelp respsonse and translate it into a feature collection
   function translate (data) {
-    const featureCollection = {
-      type: 'FeatureCollection',
-			features: []
-		}
-		// protect ourself in case the request did not return any features
-		if (data.businesses) featureCollection.features = data.businesses.map(formatFeature)
-    return featureCollection
+    // protect ourself in case the request did not return any features
+    if (data.businesses) return data.businesses.map(formatFeature)
   }
 
-	// This function takes a single element from the yelp response and translates it to GeoJSON
+  // This function takes a single element from the yelp response and translates it to GeoJSON
   function formatFeature (biz) {
     const loc = biz.location
     return {
@@ -106,11 +126,59 @@ const Yelp = function (koop) {
         rating_img: biz.rating_img_url_small,
         yelp_id: biz.id,
         is_closed: biz.is_closed,
-        term: 'String' //We put a dummy term in here so ArcGIS knows this is a string field. It will allow us to filter 
+        term: 'String' // We put a dummy term in here so ArcGIS knows this is a string field. It will allow us to filter
       }
     }
   }
   return yelp
 }
 
+function splitGeometry (bbox) {
+  return [
+    bottomLeft(bbox),
+    bottomRight(bbox),
+    topLeft(bbox),
+    topRight(bbox)
+  ]
+}
+
+function bottomLeft (bbox) {
+  return {
+    xmin: bbox.xmin,
+    ymin: bbox.ymin,
+    xmax: avgOf2(bbox.xmin, bbox.xmax),
+    ymax: avgOf2(bbox.ymin, bbox.ymax)
+  }
+}
+
+function bottomRight (bbox) {
+  return {
+    xmin: avgOf2(bbox.xmin, bbox.xmax),
+    ymin: bbox.ymin,
+    xmax: bbox.ymax,
+    ymax: avgOf2(bbox.ymin, bbox.ymax)
+  }
+}
+
+function topLeft (bbox) {
+  return {
+    xmin: bbox.xmin,
+    ymin: avgOf2(bbox.ymin, bbox.ymax),
+    xmax: avgOf2(bbox.xmin, bbox.xmax),
+    ymax: bbox.ymax
+  }
+}
+
+function topRight (bbox) {
+  return {
+    xmin: avgOf2(bbox.xmin, bbox.xmax),
+    ymin: avgOf2(bbox.ymin, bbox.ymax),
+    xmax: bbox.xmax,
+    ymax: bbox.ymax
+  }
+}
+
+function avgOf2 (a, b) {
+  return (a + b) / 2
+}
 module.exports = Yelp
